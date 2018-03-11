6168b0aeddf79e8ac3183bcf247142cbf2969aeb
Adithya Girish <adithyagirish@berkeley.edu>
2018-03-11 01:29:18 -0800

    Adding read methods and tests

--- python/ray/dataframe/test/test_io.py
+++ python/ray/dataframe/test/test_io.py
@@ -277,7 +277,11 @@ def test_from_json():
     setup_json_file(SMALL_ROW_SIZE)
 
     pd_df = pd.read_json(TEST_JSON_FILENAME)
+<<<<<<< HEAD
     ray_df = io.read_json(TEST_JSON_FILENAME)
+=======
+    ray_df = io.read_json(TEST_JSON_FILENAME, npartitions=2)
+>>>>>>> c239ff7... Adding read methods and tests
 
     assert ray_df_equals_pandas(ray_df, pd_df)
 
@@ -288,7 +292,11 @@ def test_from_html():
     setup_html_file(SMALL_ROW_SIZE)
 
     pd_df = pd.read_html(TEST_HTML_FILENAME)[0]
+<<<<<<< HEAD
     ray_df = io.read_html(TEST_HTML_FILENAME)
+=======
+    ray_df = io.read_html(TEST_HTML_FILENAME, npartitions=2)
+>>>>>>> c239ff7... Adding read methods and tests
 
     assert ray_df_equals_pandas(ray_df, pd_df)
 
@@ -299,7 +307,11 @@ def test_from_clipboard():
     setup_clipboard(SMALL_ROW_SIZE)
 
     pd_df = pd.read_clipboard()
+<<<<<<< HEAD
     ray_df = io.read_clipboard()
+=======
+    ray_df = io.read_clipboard(npartitions=2)
+>>>>>>> c239ff7... Adding read methods and tests
 
     assert ray_df_equals_pandas(ray_df, pd_df)
 
@@ -308,7 +320,11 @@ def test_from_excel():
     setup_excel_file(SMALL_ROW_SIZE)
 
     pd_df = pd.read_excel(TEST_EXCEL_FILENAME)
+<<<<<<< HEAD
     ray_df = io.read_excel(TEST_EXCEL_FILENAME)
+=======
+    ray_df = io.read_excel(TEST_EXCEL_FILENAME, npartitions=2)
+>>>>>>> c239ff7... Adding read methods and tests
 
     assert ray_df_equals_pandas(ray_df, pd_df)
 
@@ -319,7 +335,11 @@ def test_from_feather():
     setup_feather_file(SMALL_ROW_SIZE)
 
     pd_df = pd.read_feather(TEST_FEATHER_FILENAME)
+<<<<<<< HEAD
     ray_df = io.read_feather(TEST_FEATHER_FILENAME)
+=======
+    ray_df = io.read_feather(TEST_FEATHER_FILENAME, npartitions=2)
+>>>>>>> c239ff7... Adding read methods and tests
 
     assert ray_df_equals_pandas(ray_df, pd_df)
 
@@ -330,7 +350,11 @@ def test_from_hdf():
     setup_hdf_file(SMALL_ROW_SIZE)
 
     pd_df = pd.read_hdf(TEST_HDF_FILENAME, key='test')
+<<<<<<< HEAD
     ray_df = io.read_hdf(TEST_HDF_FILENAME, key='test')
+=======
+    ray_df = io.read_hdf(TEST_HDF_FILENAME, key='test', npartitions=2)
+>>>>>>> c239ff7... Adding read methods and tests
 
     assert ray_df_equals_pandas(ray_df, pd_df)
 
@@ -341,7 +365,11 @@ def test_from_msgpack():
     setup_msgpack_file(SMALL_ROW_SIZE)
 
     pd_df = pd.read_msgpack(TEST_MSGPACK_FILENAME)
+<<<<<<< HEAD
     ray_df = io.read_msgpack(TEST_MSGPACK_FILENAME)
+=======
+    ray_df = io.read_msgpack(TEST_MSGPACK_FILENAME, npartitions=2)
+>>>>>>> c239ff7... Adding read methods and tests
 
     assert ray_df_equals_pandas(ray_df, pd_df)
 
@@ -352,7 +380,11 @@ def test_from_stata():
     setup_stata_file(SMALL_ROW_SIZE)
 
     pd_df = pd.read_stata(TEST_STATA_FILENAME)
+<<<<<<< HEAD
     ray_df = io.read_stata(TEST_STATA_FILENAME)
+=======
+    ray_df = io.read_stata(TEST_STATA_FILENAME, npartitions=2)
+>>>>>>> c239ff7... Adding read methods and tests
 
     assert ray_df_equals_pandas(ray_df, pd_df)
 
@@ -363,7 +395,11 @@ def test_from_pickle():
     setup_pickle_file(SMALL_ROW_SIZE)
 
     pd_df = pd.read_pickle(TEST_PICKLE_FILENAME)
+<<<<<<< HEAD
     ray_df = io.read_pickle(TEST_PICKLE_FILENAME)
+=======
+    ray_df = io.read_pickle(TEST_PICKLE_FILENAME, npartitions=2)
+>>>>>>> c239ff7... Adding read methods and tests
 
     assert ray_df_equals_pandas(ray_df, pd_df)
 
